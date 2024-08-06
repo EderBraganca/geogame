35456791b735cc29833b8e411ab41dbd83d68a0f
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-08-05 23:30:31 -0300

    Using the Google Street View Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
 import './index.css'
 
 const App = () => {
-  const fenway = { lat: 42.345573, lng: -71.098326 };
+  const fenway = { lat: -20.385582, lng: -43.508666 };
 
   const map = new google.maps.Map(document.getElementById("map"), {
     center: fenway,
     zoom: 14,
   });
-
+  
   const panorama = new google.maps.StreetViewPanorama(
     document.getElementById("pano"),
     {
@@ -25,8 +24,7 @@ const App = () => {
   window.initialize = initialize;
   return (
     <>
-      <div id="map"></div>
-      <div id="pano"></div>
+      <button onClick={initialize} className='button'>Carregar Mapa</button>
     </>
   )
 
