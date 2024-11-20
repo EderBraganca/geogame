e664a081b82e32d90c61f8db509ad980ec90ccb8
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-11-20 13:10:43 -0300

    Make pins
    Now, we can make a singular pin at mini map

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,47 +1,73 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useLoadGoogleMaps from '../hooks/useLoadScript';
 import '../styles/map.css';
 
 const MapView = () => {
     const isLoaded = useLoadGoogleMaps();
     const mapRef = useRef(null);
+    const [marker, setMarker] = useState(null);
 
     useEffect(() => {
         if (isLoaded) {
-            const map = new google.maps.Map(
-                mapRef.current, {
-                    center: { lat: 37.7749, lng: -122.4194 },
-                    zoom: 12,
-                }
-            );
+            loadMap();
         }
     }, [isLoaded]);
 
-    // const handleMouseEnter = () => {
-    //     mapRef.current.style.width = '25vw';
-    //     mapRef.current.style.height = '30vh';
-    //     mapRef.current.style.transition = 'width 0.25s, height 0.25s';
-    // };
-    
-    // const handleMouseLeave = () => {
-    //     mapRef.current.style.width = '19vw';
-    //     mapRef.current.style.height = '25vh';
-    //     mapRef.current.style.transition = 'width 0.25s, height 0.25s';
-    // };
+    const loadMap = async () => {
+        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+        const map = new google.maps.Map(
+            mapRef.current,
+            {
+                center: { lat: 0, lng: 0 },
+                zoom: 2,
+                disableDefaultUI: true,
+                zoomControl: true,
+                mapId: 'DEMO_MAP_ID'
+            }
+        );
+
+        map.addListener("click", (event) => {
+            console.log(marker)
+            if (marker) {
+                switchMarkerPosition(event.latLng);
+            }
+            addMarker(map, event.latLng);
+        });
+
+        const addMarker = (map, location) => {
+            const dmarker = new AdvancedMarkerElement({
+                map,
+                position: location,
+                gmpDraggable: true,
+            });
+            
+            dmarker.addListener("click", (event) => {
+                // Remove AdvancedMarkerElement from Map
+                dmarker.map = null;
+            });
+
+            map.addListener("click", (event) => {
+                // Set AdvancedMarkerView position and add to Map
+                dmarker.position = event.latLng;
+                dmarker.map = map;
+            });
+            setMarker(dmarker);
+        };
+
+        const switchMarkerPosition = (location) => {
+            marker.map = null;
+            marker.position = location;
+            setMarker(marker);
+        };
+    }
 
     return (
         <div>
             {isLoaded ? (
-                <div id="map" 
-                    ref={mapRef}  
-                    // onMouseEnter={handleMouseEnter} 
-                    // onMouseLeave={handleMouseLeave}
-                >
-                </div>
-            ) 
-            : (<p className="loading-text-map">Carregando o mapa...</p>)}
+                <div id="map" ref={mapRef} />
+            ) : (<p className="loading-text-map">Carregando o mapa...</p>)}
         </div>
-  );
+    );
 };
-
 export default MapView;
