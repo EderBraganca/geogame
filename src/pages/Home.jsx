23a9b91868ca323e859826b1f2e81db3361f345e
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-12-24 15:53:26 -0300

    Create a navigate home flow

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import StartGameButton from '../components/utils/StartGameButton';
+
 const Home = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
             <h1>Home</h1>
             <StartGameButton 
                 text='Start Game' 
                 className='startGameButton' 
+                onClick={() => 
+                    navigate('/game')
+                }
             />
         </div>
     )
