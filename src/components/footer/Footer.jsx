c405bc282b30b14973bf658946a1abd6364b0b0e
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2026-02-24 21:13:26 -0300

    feat: create footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,24 @@
-import "./../../styles/footer.css"; 
+import "./../../styles/footer.css";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <footer className="footer-container">
-            Footer
+            <nav className="copyright-nav">
+                <p className="footer-text">© 2026 Eder Bragança</p>
+            </nav>
+            <nav className="github-nav">
+                <a href="https://github.com/EderBraganca" target="_blank" rel="noopener noreferrer">
+                <FaGithub size={20} color="white" className="github-icon" />
+                    <p className="footer-text">Github</p>
+                </a>
+            </nav>
+            <nav className="linkedin-nav">
+                <a href="https://linkedin.com/in/eder-braganca" target="_blank" rel="noopener noreferrer">
+                <FaLinkedinIn size={20} color="white" className="github-icon" />
+                    <p className="footer-text">Linkedin</p>
+                </a>
+            </nav>
         </footer>
     );
 };
