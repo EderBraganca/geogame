eb47fa3c6b0e1073ecc884ea6f838cf240645f58
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2026-02-24 21:21:44 -0300

    feat: rotate icons in foot animator

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,21 +1,31 @@
 import "./../../styles/footer.css";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
+    const shakeIcon = (iconClassName) => {
+        const icon = document.querySelector(iconClassName);
+        if (icon) {
+            icon.style.animation = "shake 0.5s";
+            setTimeout(() => {
+                icon.style.animation = "";
+            }, 500);
+        }
+    };
+
     return (
         <footer className="footer-container">
             <nav className="copyright-nav">
                 <p className="footer-text">© 2026 Eder Bragança</p>
             </nav>
             <nav className="github-nav">
-                <a href="https://github.com/EderBraganca" target="_blank" rel="noopener noreferrer">
-                <FaGithub size={20} color="white" className="github-icon" />
+                <a href="https://github.com/EderBraganca" target="_blank" rel="noopener noreferrer" onMouseEnter={() => shakeIcon(".github-icon")}>
+                    <FaGithub size={20} color="white" className="github-icon" />
                     <p className="footer-text">Github</p>
                 </a>
             </nav>
             <nav className="linkedin-nav">
-                <a href="https://linkedin.com/in/eder-braganca" target="_blank" rel="noopener noreferrer">
-                <FaLinkedinIn size={20} color="white" className="github-icon" />
+                <a href="https://linkedin.com/in/eder-braganca" target="_blank" rel="noopener noreferrer" onMouseEnter={() => shakeIcon(".linkedin-icon")}>
+                    <FaLinkedin size={20} color="white" className="linkedin-icon" />
                     <p className="footer-text">Linkedin</p>
                 </a>
             </nav>
